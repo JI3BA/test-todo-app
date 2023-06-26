41ac65dda08b0f68a54ce3cccf9f76f14e746e99
JI3BA <lyovkin_artyom@mail.ru>
2023-06-26 14:03:15 +0300

    changed import interface IInput

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,7 +1,7 @@
-import { Iinput } from "../Input/Input";
+import { IInput } from "../Input/Input";
 import '../../styles/Input.scss'
 
-export const Filter = ({placeholder, value, name, ...rest}: Iinput) => {
+export const Filter = ({placeholder, value, name, ...rest}: IInput) => {
     return(
         <input className='input input__filter' type='text' placeholder={placeholder} value={value} name={name} {...rest}/>
     )
