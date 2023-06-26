cd3e1cc86402c9774f2447369f7f4c5433b7e2d0
JI3BA <lyovkin_artyom@mail.ru>
2023-06-26 14:12:37 +0300

    changed import MouseEvent

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import { useNote } from "../../context";
 import '../../styles/Main.scss'
 import '../../styles/Modal.scss'
@@ -11,8 +11,9 @@ export const Modal = () => {
 
     return (
         <div>
-            {modal &&  <div className={modal ? 'modal modal__active' : 'modal'} onClick={() => setModalNotes(false)}>
-                    <div className="modal__container" onClick={(e: React.MouseEvent) => e.stopPropagation()}>
+            {modal &&
+                <div className={modal ? 'modal modal__active' : 'modal'} onClick={() => setModalNotes(false)}>
+                    <div className="modal__container" onClick={(e: MouseEvent<HTMLDivElement>) => e.stopPropagation()}>
                         <h2 className="modal__title" style={{opacity: modalNote[0].completed ? 0.5 : 1,textDecoration: modalNote[0].completed ? 'line-through' : 'none'}}>{modalNote[0].title}</h2>
                         <p className="modal__body" style={{opacity: modalNote[0].completed ? 0.5 : 1,textDecoration: modalNote[0].completed ? 'line-through' : 'none'}}>{modalNote[0].body.split(' ').map((word,index) => word.includes('#') ? <span className="note__body--tag-light" key={index}>{` ${word} `}</span> : ` ${word} `)}</p>
                         <div className="modal__tags">
