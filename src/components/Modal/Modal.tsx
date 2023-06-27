14dcf8a46739055a8193f3b86a051636694fb512
JI3BA <lyovkin_artyom@mail.ru>
2023-06-27 13:05:53 +0300

    changed map components

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,6 +4,7 @@ import '../../styles/Main.scss'
 import '../../styles/Modal.scss'
 import {Button} from '../Button/Button'
 import {MappedModalTags} from "../Map/MappedModalTags";
+import {MappedModalBody} from "../Map/MappedModalBody";
 
 
 
@@ -16,7 +17,9 @@ export const Modal = () => {
                 <div className={modal ? 'modal modal__active' : 'modal'} onClick={() => setModalNotes(false)}>
                     <div className="modal__container" onClick={(e: MouseEvent<HTMLDivElement>) => e.stopPropagation()}>
                         <h2 className="modal__title" style={{opacity: modalNote[0].completed ? 0.5 : 1,textDecoration: modalNote[0].completed ? 'line-through' : 'none'}}>{modalNote[0].title}</h2>
-                        <p className="modal__body" style={{opacity: modalNote[0].completed ? 0.5 : 1,textDecoration: modalNote[0].completed ? 'line-through' : 'none'}}>{modalNote[0].body.split(' ').map((word,index) => word.includes('#') ? <span className="note__body--tag-light" key={index}>{` ${word} `}</span> : ` ${word} `)}</p>
+                        <p className="modal__body" style={{opacity: modalNote[0].completed ? 0.5 : 1,textDecoration: modalNote[0].completed ? 'line-through' : 'none'}}>
+                            <MappedModalBody />
+                        </p>
 
                         <div className="modal__tags">
                             <MappedModalTags />
